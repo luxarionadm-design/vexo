b59eb6aec0d9b08b9e6b225b5d6beb0df4940fbd
luxarionadm-design <luxarion.adm@gmail.com>
2026-08-17 15:19:57 +0700

    Create index.js

---
+++ src/js/modules/formhandler/index.js
@@ -0,0 +1,7 @@
+/**
+ * FormHandler - Entry point
+ * Export only what's needed
+ */
+export { FormHandler, createFormHandler } from './FormHandler.js';
+export { FORM_CONFIG } from './config.js';
+export { isValidEmail, isValidPassword } from './utils/validators.js';
