a7899b2cb31a36fb746a4945b561fa4dbd0229e5
luxarionadm-design <luxarion.adm@gmail.com>
2026-08-17 15:13:58 +0700

    Add FormHandler configuration file

---
+++ src/js/modules/formhandler/config.js
@@ -0,0 +1,9 @@
+/**
+ * FormHandler Configuration
+ */
+export const FORM_CONFIG = {
+    DEMO_EMAIL: 'demo@vexorion.com',
+    DEMO_PASSWORD: 'demo123',
+    REDIRECT_URL: '/dashboard.html',
+    MIN_PASSWORD_LENGTH: 6,
+};
